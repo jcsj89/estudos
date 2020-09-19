d1d1ee888f9629f7a80050d3a95a9ae786161307
Ze Santanna <jcsj2010@gmail.com>
2020-09-18 22:01:59 -0300

    18.09.20 estudo git

--- JAVASCRIPT/C02/js/string.js
+++
@@ -1,9 +0,0 @@
-var username;
-var message;
-username = 'jose carlos';
-message = true;
-
-var elName = document.getElementById('name');
-elName.textContent = username;
-var elMessage = document.getElementById('message');
-elMessage.textContent = message;
--- JAVASCRIPT/C02/js/vector.js
+++
@@ -1,7 +0,0 @@
-var colors;
-colors = ['white','black','blue'];
-
-colors[0] = 'pppppppp';
-
-var el = document.getElementById('colors');
-el.textContent = colors[0];
