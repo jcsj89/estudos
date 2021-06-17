7f5ae9d96e75e678f9baaea312875756fae64558
= <jcsj2010@gmail.com>
2021-06-17 17:39:36 -0300

    17.06

---
+++ js/cursoManuel/secao2/app.js
@@ -0,0 +1,37 @@
+/*
+    sistema de cadastro cliente
+*/
+
+let name = "Jose";
+let lastName = "santana";
+let year = 31;
+const numClient = 1233123;
+
+console.log('boa tarde ' + name + lastName + year + numClient);
+
+//operadores aritimeticos
+let soma = 0;
+let sub = 0;
+let div = 67 / 23;
+let mod = 34 % 2;
+let prod = 34 * 45;
+let expo = 4**6;
+
+console.log('soma: '  + ++soma);
+console.log('sub: ' + sub++);
+console.log('div: '+div);
+console.log('mod: '+mod);
+console.log('prod: '+prod);
+console.log('expo: '+expo);
+
+//comparacao
+// < > <= =>
+// 
+
+console.log('&&'+ soma || sub);
+
+if (10 > 11) {
+    console.log('if');
+} else {
+    console.log('else');
+}
